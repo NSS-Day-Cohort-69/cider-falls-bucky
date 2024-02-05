c92714634eaf081f1c3f2f6695a9e2969d029ab6
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-05 16:06:30 -0600

    Detailed the basic algorithm for areas.js

--- scripts/areas.js
+++ scripts/areas.js
@@ -1,3 +1,17 @@
 /* 
-    
+    Define function to generate HTML for areas
+
+        Generate String representing HTML
+
+            Iterate through areas
+        
+                Add name of area
+
+                Add image URL of area
+            
+                Iterate through services/activities
+
+                    Add name of service/activity
+
+        Return HTML String
 */
