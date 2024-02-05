53c566af957f7ad7011cb4fb0720af7e857af12b
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-05 16:07:24 -0600

    Detailed the basic algorithm for header.js

--- scripts/header.js
+++ scripts/header.js
@@ -1,3 +1,11 @@
 /* 
-    
+    Define function to generate HTML for header
+
+        Generate HTML for header
+
+            Add park logo
+        
+            Add name of state park
+
+        Return HTML for header
 */
