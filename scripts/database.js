104f3bf83b2e4953762812c40092c228adb04636
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-05 15:28:39 -0600

    Initial commit

---
+++ scripts/database.js
@@ -0,0 +1,181 @@
+const database = {
+    areas: [
+        {
+            id: 1,
+            name: "Chamfort River",
+            imageURL: ""
+
+        },
+        {
+            id: 2,
+            name: "Lost Wolf Hiking Trail",
+            imageURL: ""
+
+        },
+        {
+            id: 3,
+            name: "Lodge",
+            imageURL: ""
+
+        },
+        {
+            id: 4,
+            name: "Gander River",
+            imageURL: ""
+
+        },
+        {
+            id: 5,
+            name: "Campgrounds",
+            imageURL: ""
+
+        },
+        {
+            id: 6,
+            name: "Pine Bluff Trails",
+            imageURL: ""
+
+        }
+    ],
+    services: [
+        {
+            id: 1,
+            name: "Rafting"
+        },
+        {
+            id: 2,
+            name: "Canoeing"
+        },
+        {
+            id: 3,
+            name: "Fishing"
+        },
+        {
+            id: 4,
+            name: "Hiking"
+        },
+        {
+            id: 5,
+            name: "Picnicking"
+        },
+        {
+            id: 6,
+            name: "Rock Climbing"
+        },
+        {
+            id: 7,
+            name: "Lodging"
+        },
+        {
+            id: 8,
+            name: "Parking"
+        },
+        {
+            id: 9,
+            name: "Information"
+        },
+        {
+            id: 10,
+            name: "Zip Lines"
+        }
+    ],
+    guests: [
+        {
+            id: ,
+            name: "",
+            areaId: 
+        }
+    ],
+    areaServices: [
+        {
+            id: 1,
+            areaId: 1,
+            serviceId: 1
+        },
+        {
+            id: 2,
+            areaId: 1,
+            serviceId: 2
+        },
+        {
+            id: 3,
+            areaId: 1,
+            serviceId: 3
+        },
+        {
+            id: 4,
+            areaId: 2,
+            serviceId: 4
+        },
+        {
+            id: 5,
+            areaId: 2,
+            serviceId: 5
+        },
+        {
+            id: 6,
+            areaId: 2,
+            serviceId: 6
+        },
+        {
+            id: 7,
+            areaId: 3,
+            serviceId: 5
+        },
+        {
+            id: 8,
+            areaId: 3,
+            serviceId: 7
+        },
+        {
+            id: 9,
+            areaId: 3,
+            serviceId: 8
+        },
+        {
+            id: 10,
+            areaId: 3,
+            serviceId: 9
+        },
+        {
+            id: 11,
+            areaId: 4,
+            serviceId: 3
+        },
+        {
+            id: 12,
+            areaId: 4,
+            serviceId: 4
+        },
+        {
+            id: 13,
+            areaId: 5,
+            serviceId: 7
+        },
+        {
+            id: 14,
+            areaId: 5,
+            serviceId: 8
+        },
+        {
+            id: 15,
+            areaId: 5,
+            serviceId: 9
+        },
+        {
+            id: 16,
+            areaId: 6,
+            serviceId: 4
+        },
+        {
+            id: 17,
+            areaId: 6,
+            serviceId: 5
+        },
+        {
+            id: 18,
+            areaId: 6,
+            serviceId: 10
+        }
+    ]
+}
---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+/* 
+    Use .querySelector to get HTML Element, and assign to variable
+
+    Initialize empty String to return later
+
+    
+*/
