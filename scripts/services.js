0447af8e54f50167d8df1f0c5753d103ff92b3c4
Jayme Menchaca-Higgins <jaymehiggins0690@gmail.com>
2024-02-07 11:51:55 -0600

    added html skeleton for services

--- scripts/services.js
+++ scripts/services.js
@@ -1,3 +1,12 @@
-/* 
+import { getServices } from "./database.js"
+
+export const serviceList = () => {
+    const services = getServices()
+
+    let HTMLstring = `<div class="service__list">`
+for (const service of services) {
+    HTMLstring = `<div class="service">${service}</div>  `
     
-*/
+}
+    HTMLstring = `</div>`
+}
